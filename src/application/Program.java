3646bdfaadcf213622c1da1fdf9275eb9005be79
Jhonny Trinca <jhonny.trinca@southsystem.com.br>
2025-12-17 16:30:40 -0300

    entidade Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
 import models.entities.Department;
+import models.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1, "Livros");
-        System.out.println(obj);
+        Seller seller = new Seller(19, "João", "joao@gmail.com", new Date(), 5000.0, obj);;
+        System.out.println(seller);
     }
 }
