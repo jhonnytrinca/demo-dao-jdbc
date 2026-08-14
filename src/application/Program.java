ea134da60d345d0961cc614f22fc0860d5ed744c
JhonnyTrinca <jhonny.trinca@hotmail.com>
2026-08-14 09:13:42 -0300

    criando função insert

--- src/application/Program.java
+++ src/application/Program.java
@@ -28,5 +28,10 @@ public class Program {
         for(Seller obj:list) {
             System.out.println(obj);
         }
+
+        System.out.println("=== TEST 4: Seller insert ===");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, dep);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserido! Novo id = " + newSeller.getId());
     }
 }
